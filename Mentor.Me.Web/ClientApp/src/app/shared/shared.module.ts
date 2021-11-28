d5178b92133251eca43a5634eea29ed03f126a19
Reverend <roma.dan2001@gmail.com>
2021-11-28 11:21:36 +0200

    Fixes

--- Mentor.Me.Web/ClientApp/src/app/shared/shared.module.ts
+++ Mentor.Me.Web/ClientApp/src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { IgxTimePickerComponent } from 'igniteui-angular';
 import AddPropositionDialogComponent from './components/add-proposition-dialog/add-proposition-dialog.component';
 import ConfirmDialogComponent from './components/confirm-dialog/confirm-dialog.component';
 import CreateMeetingDialogComponent from './components/create-meeting-dialog/create-meeting-dialog.component';
@@ -27,8 +26,7 @@ import MaterialModule from './material.module';
         MaterialModule,
         CommonModule,
         RouterModule,
-        ReactiveFormsModule,
-        IgxTimePickerComponent
+        ReactiveFormsModule
     ]
 })
 export default class SharedModule { }
