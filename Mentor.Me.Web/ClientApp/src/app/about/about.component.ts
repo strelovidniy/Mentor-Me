d4295a48bc3dc9288a60566fb6be5bff68025ac7
boredarthur <boredarthur@gmail.com>
2021-11-28 10:07:22 +0200

    about page

--- Mentor.Me.Web/ClientApp/src/app/about/about.component.ts
+++ Mentor.Me.Web/ClientApp/src/app/about/about.component.ts
@@ -6,7 +6,10 @@ import TemplateService from '../shared/services/template.service';
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.css']
 })
+
 export default class AboutComponent implements AfterViewInit {
+    public isStudent = true;
+    public isShowingSteps = false;
 
     public constructor(
         private templateService: TemplateService
@@ -14,5 +17,13 @@ export default class AboutComponent implements AfterViewInit {
 
     public async ngAfterViewInit(): Promise<void> {
         this.templateService.TurnLoaderOff();
+    }
+
+    public typeChoosed(): void {
+        this.isStudent = !this.isStudent;
+    }
+
+    public toggleSteps(): void {
+        this.isShowingSteps = true;
     }
 }
