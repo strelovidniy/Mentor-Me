7d75997e62294eba020a3914ea0f85c5f1268c0b
boredarthur <boredarthur@gmail.com>
2021-11-28 06:38:48 +0200

    about page

--- Mentor.Me.Web/ClientApp/src/app/app.component.ts
+++ Mentor.Me.Web/ClientApp/src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
-export default class AppComponent {
+export default class AppComponent implements OnInit {
     public title = 'app';
+
+    public ngOnInit(): void {
+        AOS.init();
+    }
 }
