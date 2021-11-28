9b40583e9380daa1c96bf055b58f77c54ba299b2
boredarthur <boredarthur@gmail.com>
2021-11-28 02:31:20 +0200

    header mobile menu

--- Mentor.Me.Web/ClientApp/src/app/template/template.component.ts
+++ Mentor.Me.Web/ClientApp/src/app/template/template.component.ts
@@ -6,11 +6,17 @@ import { Component } from '@angular/core';
     styleUrls: ['./template.component.css']
 })
 export default class TemplateComponent {
+    public menuOpened: boolean;
+
     public onLoaded(): void {
         document.getElementById('container').style.opacity = '1';
     }
 
     public onLoadingStarted(): void {
         document.getElementById('container').style.opacity = '0';
+    }
+
+    public mobileMenuClicked(): void {
+        this.menuOpened = !this.menuOpened;
     }
 }
